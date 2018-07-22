f5a686a2745be2c8d2b78b1a21e12432bc7069aa
sidharth01g <sidharth01g@gmail.com>
2018-07-22 23:25:02 +0530

    Added index view for first_project

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    print(request)
+    return HttpResponse("<h1>Welcome to the First App index page</h1>")
