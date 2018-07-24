f7a86196a6fcf0bfdcd65b6410a0b62833b541c6
sidharth01g <sidharth01g@gmail.com>
2018-07-24 23:38:41 +0530

    Pass context and render template

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,8 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    print(request)
-    return HttpResponse("<h1>Welcome to the First App index page</h1>")
+    context = {
+        'message': 'The first app'
+    }
+    return render(request=request, template_name='index.html', context=context)
