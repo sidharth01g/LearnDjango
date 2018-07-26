748ce3caa17b51426a80bc1d6b7af73190ab944c
sidharth01g <sidharth01g@gmail.com>
2018-07-26 22:53:23 +0530

    Bulk commit

--- second_project/my_app/admin.py
+++ second_project/my_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Topic, AccessRecord, WebPage
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(WebPage)
--- second_project/my_app/views.py
+++ second_project/my_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'message': 'Test message'
+    }
+    return render(request=request, template_name='my_app/index.html', context=context)
