e530560487e38547089a82582ab4f300ba2e3a4a
sidharth01g <sidharth01g@gmail.com>
2018-07-28 10:28:18 +0530

    Pass access record as context

--- second_project/my_app/views.py
+++ second_project/my_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from my_app.models import AccessRecord
+
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    context = {
-        'message': 'Test message'
-    }
+    webpages_list = AccessRecord.objects.order_by('date')
+    context = {'access_records': webpages_list}
     return render(request=request, template_name='my_app/index.html', context=context)
