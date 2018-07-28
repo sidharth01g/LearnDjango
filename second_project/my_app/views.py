10e6ee8bcf788b1f46ae67a5af286b90b718e523
sidharth01g <sidharth01g@gmail.com>
2018-07-28 17:38:19 +0530

    Added endpoint to display users

--- second_project/my_app/views.py
+++ second_project/my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from my_app.models import AccessRecord
+from my_app.models import AccessRecord, User
 
 
 # Create your views here.
@@ -8,3 +8,10 @@ def index(request: HttpRequest) -> HttpResponse:
     webpages_list = AccessRecord.objects.order_by('date')
     context = {'access_records': webpages_list}
     return render(request=request, template_name='my_app/index.html', context=context)
+
+def users(request: HttpRequest) -> HttpResponse:
+    users_list = User.objects.order_by('email')
+    context = {
+        'users': users_list
+    }
+    return render(request=request, template_name='my_app/users.html', context=context)
