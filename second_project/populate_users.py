bb43283a582edf4c088f3d24487a427c0b49cbfc
sidharth01g <sidharth01g@gmail.com>
2018-07-29 08:35:11 +0530

    Reorder imports

--- second_project/populate_users.py
+++ second_project/populate_users.py
@@ -1,18 +1,20 @@
 from faker import Faker
-from my_app.models import User
+
 import os
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'second_project.settings')
 django.setup()
+from my_app.models import User
 
 fakegen = Faker()
 
 
 def populate(n_records: int):
     for index in range(n_records):
-        first_name = fakegen.name()
-        last_name = fakegen.name()
+        name = fakegen.name().split()
+        first_name = name[0]
+        last_name = name[-1]
         email = fakegen.email()
         user = User.objects.get_or_create(first_name=first_name, last_name=last_name, email=email)[0]
         user.save()
