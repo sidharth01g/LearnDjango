2dee6ce24248f9b065352e042fe399d8e61bee2d
sidharth01g <sidharth01g@gmail.com>
2018-07-29 17:33:06 +0530

    Updated URL

--- second_project/my_app/urls.py
+++ second_project/my_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^access_records/$', views.index, name='index'),
     url(r'^users/$', views.users, name='users'),
+    url(regex=r'^create_user/$', view=views.create_user, name='create_user')
 ]
