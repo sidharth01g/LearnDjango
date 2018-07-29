4628f39a28894726758f26eead470ef7d846d61e
sidharth01g <sidharth01g@gmail.com>
2018-07-29 17:33:32 +0530

    Use model-based form

--- second_project/my_app/views.py
+++ second_project/my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from my_app.models import AccessRecord, User
+from .forms import NewUserForm
 
 
 # Create your views here.
@@ -9,9 +10,28 @@ def index(request: HttpRequest) -> HttpResponse:
     context = {'access_records': webpages_list}
     return render(request=request, template_name='my_app/index.html', context=context)
 
+
 def users(request: HttpRequest) -> HttpResponse:
     users_list = User.objects.order_by('email')
     context = {
         'users': users_list
     }
     return render(request=request, template_name='my_app/users.html', context=context)
+
+
+def create_user(request: HttpRequest) -> HttpResponse:
+    form = NewUserForm()
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR: Form validation failed')
+
+    context = {
+        'user_form': form
+    }
+
+    return render(request=request, template_name='my_app/create_user.html', context=context)
