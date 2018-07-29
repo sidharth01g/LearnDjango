3f33013f18c717b5a2e980819b8a79dc3197c54a
sidharth01g <sidharth01g@gmail.com>
2018-07-29 15:13:20 +0530

    Added views

--- level_three/basicforms/basicapp/views.py
+++ level_three/basicforms/basicapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from . import forms
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'message': 'Test'
+    }
+    return render(request=request, template_name='basicapp/index.html', context=context)
+
+
+def user_input(request: HttpRequest) -> HttpResponse:
+    context = {
+        'form': forms.UserForm()
+    }
+    if request.method == 'POST':
+        form = forms.UserForm(request.POST)
+        if form.is_valid():
+            print('Form validated')
+            print('Name: {}'.format(form.cleaned_data['name']))
+            print('Email: {}'.format(form.cleaned_data['email']))
+            print('Description: {}'.format(form.cleaned_data['description']))
+    return render(request=request, template_name='basicapp/form_page.html', context=context)
