0107c04d622a1805b5d921dc03b3fcf31e607a2b
sidharth01g <sidharth01g@gmail.com>
2018-08-10 21:56:33 +0530

    Added URLS

--- level_five/learning_users/learning_users/urls.py
+++ level_five/learning_users/learning_users/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from basic_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(regex=r'^$', view=views.index, name='index'),
+    url(regex=r'^admin/', view=admin.site.urls),
+    url(regex=r'^basic_app/', view=include('basic_app.urls')),
 ]
