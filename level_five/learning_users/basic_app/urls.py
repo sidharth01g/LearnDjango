fcbdc59175e24478c761ac0ee7a319ee5f6f9134
sidharth01g <sidharth01g@gmail.com>
2018-08-11 13:58:22 +0530

    Added login and logout

--- level_five/learning_users/basic_app/urls.py
+++ level_five/learning_users/basic_app/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'basic_app'
 
 urlpatterns = [
+    # url 'name' parameter is used within HTML when we user,
+    # for example {% url 'basic_app:user_login' %}
+    url(regex=r'^logout', view=views.user_logout, name='user_logout'),
+    url(regex=r'^login/', view=views.user_login, name='user_login'),
     url(regex=r'^register/', view=views.register, name='register'),
 
 ]
