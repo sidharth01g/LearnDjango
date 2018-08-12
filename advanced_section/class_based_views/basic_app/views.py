a78bae3d02c6c018de73a94e34e6199bf9e62c4b
sidharth01g <sidharth01g@gmail.com>
2018-08-12 09:34:04 +0530

    Moved from method-based view to class-based view

--- advanced_section/class_based_views/basic_app/views.py
+++ advanced_section/class_based_views/basic_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import View
 
 
-# Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name='basic_app/index.html', context={})
+class AppView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request=request, template_name='basic_app/index.html', context={})
