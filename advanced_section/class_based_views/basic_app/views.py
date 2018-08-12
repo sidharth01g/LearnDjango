9e000f01da3122aca449474d68681f5d02340d69
sidharth01g <sidharth01g@gmail.com>
2018-08-12 10:39:07 +0530

    Inject context using TemplateView

--- advanced_section/class_based_views/basic_app/views.py
+++ advanced_section/class_based_views/basic_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-from django.views.generic import View, TemplateView
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'basic_app/index.html'
 
-
-
-
+    def get_context_data(self, **kwargs) -> HttpResponse:
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This page uses injected context my means of TempateViews'
+        return context
