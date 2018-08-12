04e2b02e14416270ba344051a7f246cffb9ecb42
sidharth01g <sidharth01g@gmail.com>
2018-08-12 21:06:01 +0530

    Added ListView and DetailView classes for schools

--- advanced_section/class_based_views/basic_app/views.py
+++ advanced_section/class_based_views/basic_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from . import models
 
 
 class IndexView(TemplateView):
@@ -9,3 +10,25 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['message'] = 'This page uses injected context my means of TempateViews'
         return context
+
+
+class SchoolListView(ListView):
+    # ListView: context variable names
+    # ================================
+    # By default Django injects "model.School" into context via a list named "school_list" in the
+    # HTML template
+    # Django lowercases the variable name "School" to "school" and appends "_list" -> "school_list"
+    # This variable is a list of all schools in the database
+    # We can modify this default behavior by naming the list variable ourselves by using a
+    # variable "context_object_name"
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    # DetailView: context varible names
+    # =================================
+    # Djanog lowercases "models.School" to "school"
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
