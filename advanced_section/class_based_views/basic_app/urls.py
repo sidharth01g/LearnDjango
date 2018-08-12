5197ff7bb3892d565cbc1d84a476c39052572f4e
sidharth01g <sidharth01g@gmail.com>
2018-08-12 21:05:13 +0530

    Added List View of schools, added app_name

--- advanced_section/class_based_views/basic_app/urls.py
+++ advanced_section/class_based_views/basic_app/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'basic_app'
+
 urlpatterns = [
     url(regex=r'^$', view=views.IndexView.as_view()),
+    url(regex=r'^school_list/', view=views.SchoolListView.as_view(), name='school_list'),
 ]
