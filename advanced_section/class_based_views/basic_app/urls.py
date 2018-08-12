0b9a9a781aaf9e9c62c8ad86f2ac237e0d94d51e
sidharth01g <sidharth01g@gmail.com>
2018-08-12 22:55:59 +0530

    Added URl regex for school ID parsing using named regex groups

--- advanced_section/class_based_views/basic_app/urls.py
+++ advanced_section/class_based_views/basic_app/urls.py
@@ -5,5 +5,6 @@ app_name = 'basic_app'
 
 urlpatterns = [
     url(regex=r'^$', view=views.IndexView.as_view()),
-    url(regex=r'^school_list/', view=views.SchoolListView.as_view(), name='school_list'),
+    url(regex=r'^school_list/(?P<pk>[-\w]+)$', view=views.SchoolDetailView.as_view(), name='school_detail'),
+    url(regex=r'^school_list/$', view=views.SchoolListView.as_view(), name='school_list'),
 ]
