c7ea21891a47dfead3296411021b1cc1ec2a3496
sidharth01g <sidharth01g@gmail.com>
2018-08-12 18:35:22 +0530

    Added models

--- advanced_section/class_based_views/basic_app/models.py
+++ advanced_section/class_based_views/basic_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=500)
+    principal = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=500)
+    age = models.IntegerField()
+    school = models.ForeignKey(to=School, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.name
