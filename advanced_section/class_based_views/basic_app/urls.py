82f4fa377d5b0b7f2e3476a59b9c7903c32c8901
sidharth01g <sidharth01g@gmail.com>
2018-08-18 10:45:51 +0530

    Added URL to create a school

--- advanced_section/class_based_views/basic_app/urls.py
+++ advanced_section/class_based_views/basic_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(regex=r'^$', view=views.IndexView.as_view()),
     url(regex=r'^school_list/(?P<pk>[-\w]+)$', view=views.SchoolDetailView.as_view(), name='school_detail'),
     url(regex=r'^school_list/$', view=views.SchoolListView.as_view(), name='school_list'),
+    url(regex=r'^create/$', view=views.SchoolCreateView.as_view(), name='school_create'),
 ]
