bdd39f829bd1073251fc2876b40a3dfc341542f0
sidharth01g <sidharth01g@gmail.com>
2018-08-18 12:26:10 +0530

    Added view class to update schools

--- advanced_section/class_based_views/basic_app/urls.py
+++ advanced_section/class_based_views/basic_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(regex=r'^$', view=views.IndexView.as_view()),
     url(regex=r'^school_list/(?P<pk>[-\w]+)$', view=views.SchoolDetailView.as_view(), name='school_detail'),
     url(regex=r'^school_list/$', view=views.SchoolListView.as_view(), name='school_list'),
-    url(regex=r'^create/$', view=views.SchoolCreateView.as_view(), name='school_create'),
+    url(regex=r'^school_create/$', view=views.SchoolCreateView.as_view(), name='school_create'),
+    url(regex=r'^school_update/(?P<pk>[-\w]+)$', view=views.SchoolUpdateView.as_view(), name='school_update'),
 ]
