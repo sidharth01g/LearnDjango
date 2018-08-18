196de54edc18c19a92e625e727a4a550a88cd699
sidharth01g <sidharth01g@gmail.com>
2018-08-18 10:45:32 +0530

    Added get_absolute_url method to reverse-return URL from model

--- advanced_section/class_based_views/basic_app/models.py
+++ advanced_section/class_based_views/basic_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -9,6 +10,15 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        """
+        Get URL from view/model
+
+        Returns:
+            URL
+        """
+        return reverse(viewname='basic_app:school_create')
 
 
 class Student(models.Model):
